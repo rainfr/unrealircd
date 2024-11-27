6c98f7224a68d26f7d451a570bee6afb4e27cdc2
Bram Matthys <syzop@vulnscan.org>
2024-11-27 10:19:53 +0100

    Always try to maintain chronological order in chat history, and optimize stuff.
    Previously if a new history item was added (because someone sent a message)
    we would always append at the end of chat history buffer of the channel.
    Now we put the message at the position decided by the "time" message tag,
    which could be at the end but also slightly before that.
    * Upside: should result in a consistent chat history on all servers
    * Downside: if your server time is off for several seconds then it
      could look a little weird. Then again, it would already have looked weird
      in real live chat with timestamps and when replaying chat history probably.
    
    Also add some simple optimizations: in the log line object we now have direct
    pointers to the msgid and time strings, so the code doesn't need to do a
    find_mtag() all the time. This should lower CPU usage during log playback
    and also makes things more simple in the source code.
    
    I did some testing with various history injection variants but this needs
    more extensive testing.

--- include/modules.h
+++ include/modules.h
@@ -582,9 +582,11 @@ struct HistoryFilter {
 typedef struct HistoryLogLine HistoryLogLine;
 struct HistoryLogLine {
 	HistoryLogLine *prev, *next;
-	time_t t;
-	MessageTag *mtags;
-	char line[1];
+	time_t t;		/**< Rounded time on seconds, for quick access. */
+	char *msgid;		/**< Pointer to 'msgid' mtag. Do NOT free this, it is freed by freeing 'mtags'. */
+	char *time;		/**< Pointer to 'time' mtag. Do NOT free this, it is freed by freeing 'mtags'. */
+	MessageTag *mtags;	/**< Message tags associated with this message */
+	char line[1];		/**< The full (old-skool) IRC protocol line */
 };
 
 typedef struct HistoryResult HistoryResult;
